872d76c3d1c87fcdf875ed5162b8fd2951ddb41c
laranjeira <maat.laranjeira@gmail.com>
2025-07-06 18:40:49 -0300

    Containers to/from DB
    +container schema, container model

--- models/Task.js
+++ models/Task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  taskContainer: String,
+  containerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Container' },
   titulo: String,
   descricao: String,
   dataLimite: Date,
